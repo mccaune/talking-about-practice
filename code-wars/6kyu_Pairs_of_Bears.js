4422b9cb0c23dd08a51d30902bf291f9ff1dd9d2
mc <mairis.caune@gmail.com>
2021-02-09 21:43:17 +0200

    adding compleated challanges

---
+++ code-wars/6kyu_Pairs_of_Bears.js
@@ -0,0 +1,26 @@
+/*
+In order to prove it's success and gain funding, the wilderness zoo needs to prove to environmentalists that it has x number of mating pairs of bears.
+
+You must check within string (s) to find all of the mating pairs, and return a string containing only them. Line them up for inspection.
+
+Rules: Bears are either 'B' (male) or '8' (female), Bears must be together in male/female pairs 'B8' or '8B', Mating pairs must involve two distinct bears each ('B8B' may look fun, but does not count as two pairs).
+
+Return an array containing a string of only the mating pairs available. e.g:
+
+'EvHB8KN8ik8BiyxfeyKBmiCMj' ---> 'B88B' (empty string if there are no pairs)
+
+and true if the number is more than or equal to x, false if not:
+
+(6, 'EvHB8KN8ik8BiyxfeyKBmiCMj') ---> ['B88B', false];
+*/
+
+function bears(x, s){
+  final_str = '';
+  for(let i = 0; i < s.length; i++){
+    if ((s[i] == 'B' && s[i+1] == '8') || (s[i] == '8' && s[i+1] == 'B') ){
+      final_str = final_str.concat(s[i]+s[i+1]);
+      i++;
+    } 
+  } 
+  return x <= final_str.length/2 ? [final_str, true] : [final_str, false];
+}
